5afdfc4dfa996f4eab15fe4e0f72fcc3f4725e3f
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-02-20 21:19:38 -0500

    init

---
+++ src/main/java/com/example/devops/lab2/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.devops.lab2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("")
+    public String helloWorld() {
+        return "Welcome to COMP367";
+    }
+}
---
+++ src/main/java/com/example/devops/lab2/Lab2Application.java
@@ -0,0 +1,16 @@
+package com.example.devops.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Lab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2Application.class, args);
+    }
+
+}
+
