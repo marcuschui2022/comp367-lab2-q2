473094ee29d136694ec8a8a706f9302417b6ef43
Kwok Yuk Chui <kchui8@my.centennialcollege.ca>
2024-02-20 22:14:20 -0500

    bug fix

--- src/main/java/com/example/devops/lab2/HelloWorldController.java
+++ src/main/java/com/example/devops/lab2/HelloWorldController.java
@@ -3,11 +3,18 @@ package com.example.devops.lab2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RestController
 public class HelloWorldController {
-
     @GetMapping("")
     public String helloWorld() {
-        return "Good afternoon, Kwok Yuk Chui";
+        LocalTime currentTime = LocalTime.now();
+
+        if (currentTime.isAfter(LocalTime.NOON)) {
+            return "Good afternoon, Kwok Yuk Chui";
+        } else {
+            return "Good morning, Kwok Yuk Chui";
+        }
     }
 }
